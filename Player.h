8ebf1de541240dc1afbc07a37b7186cca6152fe0
RyanHans-La <59070902+RyanHans-La@users.noreply.github.com>
2023-08-02 20:30:07 +0930

    Add files via upload

---
+++ Player.h
@@ -0,0 +1,12 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+
+class Player {
+public:
+    virtual Move* makeMove() = 0;
+    virtual std::string getName() = 0;
+};
+
+#endif // PLAYER_H
